021cdf28f7d4aa420c461b0b618b232bc3778305
Tsirov <anton121896@gmail.com>
2022-04-08 16:38:47 +0300

    Added history page

--- src/components/history/history.js
+++ src/components/history/history.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
+import './history.css'
 
 const History = (props) => {
     const currentProvider = props.provider;
@@ -9,57 +10,59 @@ const History = (props) => {
     const [address, setAddress] = useState('');
     const [sendPayments, setSendPayments] = useState([]);
     const [receivePayments, setReceivePayments] = useState([]);
-    console.log('provider', currentProvider);
+
+    // useEffect(() => {
+    //     if (currentProvider) {
+    //         const ethProvider = new ethers.providers.Web3Provider(currentProvider);
+    //         const currentSigner = ethProvider.getSigner();
+    //         setSigner(currentSigner)
+    //         setProvider(ethProvider)
+
+    //     }
+    // }, [currentProvider]);
 
     useEffect(() => {
         if (currentProvider) {
-            const ethProvider = new ethers.providers.Web3Provider(currentProvider);
-            const currentSigner = ethProvider.getSigner();
-            setSigner(currentSigner)
-            setProvider(ethProvider)
+            async function start() {
+                try {
+                    const ethProvider = new ethers.providers.Web3Provider(currentProvider);
+                    const currentSigner = ethProvider.getSigner();
+                    setSigner(currentSigner)
+                    setProvider(ethProvider)
+                    const network = await ethProvider.getNetwork();
+                    const networkProvider = new ethers.providers.EtherscanProvider(network.name)
+                    const currentAddress = await currentSigner.getAddress();
+                    setAddress(currentAddress)
+                    let currentHistory = await networkProvider.getHistory(currentAddress);
+                    setHistory(currentHistory);
+                } catch (err) {
+                    console.log(err);
+                }
+        
+            }
+            start();
 
         }
     }, [currentProvider]);
 
-    // useEffect(() => {
-    //     if (history.length > 0) {
-    //         console.log('in if OOOOO', history);
-    //         for (let i = history.length; i > 0; i--){
-    //             const obj = history[i - 1];
-
-    //             if (obj.from === address) {
-    //                 setSendPayments(prev => [...prev,obj]);
-    //             } else {
-    //                 setReceivePayments(prev => [...prev,obj]);
-    //             }
-    //         }
-           
+    
+    // async function start() {
+    //     try {
+    //         const network = await provider.getNetwork();
+    //         const networkProvider = new ethers.providers.EtherscanProvider(network.name)
+    //         const currentAddress = await signer.getAddress();
+    //         setAddress(currentAddress)
+    //         let currentHistory = await networkProvider.getHistory(currentAddress);
+    //         setHistory(currentHistory);
+    //     } catch (err) {
+    //         console.log(err);
     //     }
-    // }, [history])
 
-   
-    async function start() {
-        try {
-            const network = await provider.getNetwork();
-            console.log(network);
-            const networkProvider = new ethers.providers.EtherscanProvider(network.name)
-            const currentAddress = await signer.getAddress();
-            setAddress(currentAddress)
-            // console.log(1);
-            let currentHistory = await networkProvider.getHistory(currentAddress);
-            setHistory(currentHistory);
-            console.log(history);
-            console.log(history[0].from);
-        } catch (err) {
-            console.log(err);
-        }
-
-    }
+    // }
 
     const clickHandlerSend = () => {
-        start();
+        // start();
         if (history.length > 0) {
-            console.log('in if OOOOO', history);
             for (let i = history.length; i > 0; i--) {
                 const obj = history[i - 1];
 
@@ -74,11 +77,10 @@ const History = (props) => {
     };
 
     const clickHandlerReceive = () => {
-        console.log(history);
 
-        start();
+        // start();
         if (history.length > 0) {
-            console.log('in if OOOOO', history);
+            console.log('inside');
             for (let i = history.length; i > 0; i--) {
                 const obj = history[i - 1];
 
@@ -93,17 +95,37 @@ const History = (props) => {
 
     const sendView = () => {
         console.log('senview');
-        return(
-            sendPayments.map((obj) => {
+        return (
+            sendPayments.map((obj, i) => {
                 return (
-                    <section>
-                        <div>
+                    <section key={i} className="history-second-wrapper">
+                        <div className="history-address">
                             <h1>To Address:</h1> 
                             <p>{obj.to }</p>
                         </div>
-                        <div>
+                        <div className="history-amount">
                             <h1>Amount:</h1>
                             <p>{ ethers.utils.formatEther(obj.value) }</p>
+                        </div>
+                    </section>
+                )
+            })
+        )
+    };
+
+    const receiveView = () => {
+        console.log('senview',receivePayments );
+        return(
+            receivePayments.map((obj, i) => {
+                return (
+                    <section key={ i } className="history-second-wrapper">
+                        <div className="history-address">
+                            <h1>To Address:</h1> 
+                            <p>{obj.to }</p>
+                        </div>
+                        <div className="history-amount">
+                            <h1>Amount:</h1>
+                            <p>{ ethers.utils.formatEther(obj.value)}</p>
                         </div>
                     </section>
                 )
@@ -113,7 +135,7 @@ const History = (props) => {
    
     console.log('anton',sendPayments);
     return (
-        <>
+        <section className="history-wrapper">
             {/* <section>
                 { history.map((obj) => {
                     return (
@@ -121,11 +143,15 @@ const History = (props) => {
                     )
                 })}
             </section> */}
-            <button onClick={ clickHandlerSend }> Send Transaction</button>
-            <button onClick={ clickHandlerReceive }> Receive Transaction</button>
+            <section className="history-wrapper-buttons">
+            <button className="history-bottom" onClick={ clickHandlerSend }> Send Transaction</button>
+            <button className="history-bottom" onClick={ clickHandlerReceive }> Receive Transaction</button>
+            </section>
+            
             {sendPayments.length > 0 ? sendView() : '' }
+            {receivePayments.length > 0 ? receiveView() : '' }
 
-        </>
+        </section>
 
     )
 }
